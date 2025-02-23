50e551a7abcf9739fb3d80ca1fb026d3c6d40df4
Michael Dyer <michaelowendyer@gmail.com>
2025-02-23 16:32:22 +0100

    Add gaming settings

--- system/modules/nixos/gaming.nix
+++ system/modules/nixos/gaming.nix
@@ -7,21 +7,24 @@
 
 {
   # Declare the option to enable or disable Steam
-  options.games.steam.enable = lib.mkEnableOption "Steam";
+  options.gaming.enable = lib.mkEnableOption "Gaming";
 
   config =
     let
-      cfg = config.games.steam;
+      cfg = config.gaming;
     in
     lib.mkIf cfg.enable {
       # Configure Steam
-      hardware.steam-hardware.enable = true;
-      programs.steam = {
-        enable = true;
-        remotePlay.openFirewall = false; # Set true to open ports in the firewall for Steam Remote Play
-      };
+      programs.steam.enable = true;
+      programs.steam.gamescopeSession.enable = true;
+      # https://search.nixos.org/options?channel=unstable&show=programs.gamemode.enable
+      programs.gamemode.enable = true;
       environment.systemPackages = with pkgs; [
         wine # For running Windows games on Linux
+        mangohud # In-game overlay
+        lutris # Steam alternative
+        heroic # Steam alternative
+        bottles # Run Windows games directly
       ];
     };
 }
