1707eaaac3e5cd5ff2754f3fe323ebeab990ca2c
Michael Dyer <michaelowendyer@gmail.com>
2025-02-25 23:14:33 +0100

    Document util

--- util.nix
+++ util.nix
@@ -1,4 +1,8 @@
 {
+  # This is a helper function to create an option in the form profiles.<username>.<option>,
+  # e.g. an option which can be configured independently for each user profile.
+  # For now, it requires "lib" to be passed as an argument.
+  # TODO: Investigate how to make this function available without passing "lib" as an argument
   mkProfileOption =
     lib: option:
     {
