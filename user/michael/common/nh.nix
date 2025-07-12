69568036f7b0abddad272fd21516223034a734a7
Michael Dyer <michaelowendyer@gmail.com>
2025-07-12 23:07:22 +0200

    fix nh env var NH_FLAKE

--- user/michael/common/nh.nix
+++ user/michael/common/nh.nix
@@ -9,11 +9,11 @@
 {
   programs.nh = {
     enable = true;
+    flake = "${homeDirectory}/.dotfiles";
     clean = {
       enable = true;
       dates = "weekly";
       extraArgs = "--keep-since 7d --keep 7";
     };
   };
-  home.sessionVariables.NH_FLAKE = "${homeDirectory}/.dotfiles";
 }
