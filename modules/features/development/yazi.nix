4a1c1f5a6740e6a0b928ea7029d89def53d24c52
Michael Dyer <michael.dyer@check24.de>
2026-01-09 22:38:57 +0100

    Configure yazi

--- modules/features/development/yazi.nix
+++ modules/features/development/yazi.nix
@@ -8,11 +8,47 @@
       ...
     }:
     {
-      programs.yazi.enable = true;
-      # Enable EXIF tool
-      home.packages = with pkgs; [
-        exiftool
-      ];
+      programs.yazi = {
+        enable = true;
+        settings = {
+          yazi = {
+            ratio = [
+              1
+              4
+              3
+            ];
+            sort_by = "natural";
+            sort_sensitive = true;
+            sort_reverse = false;
+            sort_dir_first = true;
+            linemode = "none";
+            show_hidden = true;
+            show_symlink = true;
+          };
+
+          preview = {
+            image_filter = "lanczos3";
+            image_quality = 90;
+            tab_size = 1;
+            max_width = 600;
+            max_height = 900;
+            cache_dir = "";
+            ueberzug_scale = 1;
+            ueberzug_offset = [
+              0
+              0
+              0
+              0
+            ];
+          };
+
+          tasks = {
+            micro_workers = 5;
+            macro_workers = 10;
+            bizarre_retry = 5;
+          };
+        };
+      };
       # enable y shell wrapper that provides the ability to
       # change the current working directory when exiting Yazi.
       programs.fish.interactiveShellInit = ''
@@ -43,5 +79,9 @@
           rm -f -- "$tmp"
         }
       '';
+      # Enable EXIF tool
+      home.packages = with pkgs; [
+        exiftool
+      ];
     };
 }
