087277984caa681b78183add7548c1c475d579b1
michael <michaelowendyer@gmail.com>
2026-01-12 23:29:54 +0100

    add new ssh key for rustbucket

--- modules/users/michael/nixos.nix
+++ modules/users/michael/nixos.nix
@@ -7,9 +7,9 @@ let
   email = "michaelowendyer@gmail.com";
   defaultHashedPassword = "$y$j9T$pSkVWxgO/9dyqt8MMHzaM0$RO5g8OOpFb4pdgMuDIVraPvsLMSvMTU2/y8JQWfmrs1";
   trustedSshKeys = [
-    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINj8NsgxdKFSwaLN5cK2vebcfdWTI39OoAutnMI9awyD michael@rustbucket"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAINtKFTvyCJo4u7KstzHIZ/ZdBCfS5ukmItX75tC0aM5O michael@claptrap"
     "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIE1RmSKMqa9vAqyHjJzEmjFOJYV+qT/imHpXDmeWl6PI michael@rpi-3b"
+    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIHvS0a2EKa9kxQ1DK3JMS6UVYOXnOvlTFQzukp9U/M4n michael@rustbucket"
   ];
 in
 {
