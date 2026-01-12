02368d33f7050531496bf95d8387ca741c0dd5c1
michael <michaelowendyer@gmail.com>
2026-01-12 23:29:44 +0100

    fix ripgrep

--- modules/features/development/ripgrep.nix
+++ modules/features/development/ripgrep.nix
@@ -5,7 +5,7 @@
   # ripgrep: grep alternative written in Rust
 
   flake.modules.nixos.ripgrep =
-    { ... }:
+    { pkgs, ... }:
     {
       environment.systemPackages = [ pkgs.ripgrep ];
     };
@@ -17,7 +17,7 @@
     };
 
   flake.modules.homeManager.ripgrep =
-    { ... }:
+    { pkgs, ... }:
     {
       home.packages = [ pkgs.ripgrep ];
     };
