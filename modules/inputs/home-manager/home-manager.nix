06af1bbda4c7f136d1fcb5c6e1137f8c1be2b783
Michael Dyer <michael.dyer@check24.de>
2026-01-12 19:03:22 +0100

    Restructure home-manager.nix

--- modules/inputs/home-manager/home-manager.nix
+++ modules/inputs/home-manager/home-manager.nix
@@ -3,19 +3,17 @@
   ...
 }:
 let
-  home-manager-config = {
-    home-manager = {
-      verbose = true;
-      # Changes the home-manager package installation path
-      # from $HOME/.nix-profile to /etc/profiles.
-      useUserPackages = true;
-      # Inherit nixpkgs instance from NixOS.
-      # This saves us an expensive second evaluation of nixpkgs
-      # when rebuilding NixOS and home-manager together,
-      # at the expense of higher coupling.
-      useGlobalPkgs = true;
-      backupFileExtension = "backup";
-    };
+  home-manager = {
+    verbose = true;
+    # Changes the home-manager package installation path
+    # from $HOME/.nix-profile to /etc/profiles.
+    useUserPackages = true;
+    # Inherit nixpkgs instance from NixOS.
+    # This saves us an expensive second evaluation of nixpkgs
+    # when rebuilding NixOS and home-manager together,
+    # at the expense of higher coupling.
+    useGlobalPkgs = true;
+    backupFileExtension = "backup";
   };
 in
 {
@@ -23,14 +21,14 @@ in
   flake.modules.nixos.home-manager = {
     imports = [
       inputs.home-manager.nixosModules.home-manager
-      home-manager-config
     ];
+    inherit home-manager;
   };
 
   flake.modules.darwin.home-manager = {
     imports = [
       inputs.home-manager.darwinModules.home-manager
-      home-manager-config
     ];
+    inherit home-manager;
   };
 }
