df12665febf3e86f31aeb36f997c334ae2267ca7
Michael Dyer <michaelowendyer@gmail.com>
2026-01-24 15:30:49 +0100

    fix warnings

--- modules/settings/wifi.nix
+++ modules/settings/wifi.nix
@@ -27,13 +27,15 @@
     # DNS with caching and DNSSEC
     services.resolved = {
       enable = true;
-      dnssec = "allow-downgrade";
-      fallbackDns = [
-        "1.1.1.1"
-        "8.8.8.8"
-        "2606:4700:4700::1111"
-        "2001:4860:4860::8888"
-      ];
+      settings.Resolve = {
+        DNSSEC = "allow-downgrade";
+        FallbackDNS = [
+          "1.1.1.1"
+          "8.8.8.8"
+          "2606:4700:4700::1111"
+          "2001:4860:4860::8888"
+        ];
+      };
     };
   };
 }
