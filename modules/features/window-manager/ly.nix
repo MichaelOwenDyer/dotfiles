657ec4d27ab5a6bfb757fd105c5eff79b6aece7a
michael <michaelowendyer@gmail.com>
2026-02-09 23:15:52 +0100

    nix flake update

--- modules/features/window-manager/ly.nix
+++ modules/features/window-manager/ly.nix
@@ -5,9 +5,14 @@
   # Minimalist ly login manager
 
   flake.modules.nixos.ly =
-    { ... }:
+    { pkgs, ... }:
     {
-      services.displayManager.ly.enable = true;
+      services.displayManager.ly = {
+        enable = true;
+        # Use master nixpkgs version to work around build failure in unstable
+        # (ln: failed to create symbolic link '/p': Permission denied)
+        package = pkgs.master.ly;
+      };
 
       # Impermanence: persist last selected user/session
       impermanence.persistedFiles = [ "/etc/ly/save.txt" ];
