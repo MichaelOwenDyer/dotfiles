d83616a8d7355f828d59c486af988f84e18e1c0d
michael <michaelowendyer@gmail.com>
2026-02-11 18:00:27 +0100

    configure nethack

--- modules/features/nethack.nix
+++ modules/features/nethack.nix
@@ -3,8 +3,29 @@
 }:
 {
   flake.modules.homeManager.nethack =
-  { pkgs, ... }:
-  {
-    home.packages = [ pkgs.nethack ];
-  };
+    { pkgs, ... }:
+    {
+      home.packages = [ pkgs.nethack ];
+
+      # NetHack configuration file
+      home.file.".nethackrc".text = ''
+        # Ask for player name every time
+        OPTIONS=name:player
+        # number_pad:1 enables arrow keys for movement
+        OPTIONS=number_pad:1
+
+        # Display boulders as '0' instead of the default backtick
+        OPTIONS=boulder:0
+
+        OPTIONS=autopickup
+        # Only autopickup money, scrolls, potions, and wands
+        OPTIONS=pickup_types:$?!/
+        OPTIONS=color
+        OPTIONS=showexp
+        OPTIONS=time
+        OPTIONS=autodig
+        OPTIONS=pushweapon
+        OPTIONS=disclose:yi ya yv yg yc yo
+      '';
+    };
 }
