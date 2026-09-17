ec2c625a9aa6ec350f0812d636accbb4c5d97a54
Michael Dyer <michaelowendyer@gmail.com>
2026-09-18 00:21:40 +0200

    fix adguardhome config

--- modules/hosts/router/configuration.nix
+++ modules/hosts/router/configuration.nix
@@ -304,7 +304,7 @@
 
         services.adguardhome.settings = {
           dns.bind_hosts = [ lanAddress ];
-          filtering.rewrites =
+          rewrites =
             inputs.self.lib.hosts
             |> lib.mapAttrsToList (
               _name: host:
@@ -333,7 +333,7 @@
               lib.optionals (homeNet != null && (homeNet.ipv4 or null) != null) [
                 {
                   name = host.hostName;
-                  ids = [ homeNet.ipv4 ] ++ lib.optional ((homeNet.mac or null) != null) homeNet.mac;
+                  ids = [ homeNet.ipv4 ];
                 }
               ]
             )
